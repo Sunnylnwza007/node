8f50de99fef8d9a86e935a6f08397b1184fbcabc
Trinnakrit <trinnakrit.p@clicknext.com>
2020-07-17 17:54:01 +0700

    eiei

--- Controller/threads.js
+++ Controller/threads.js
@@ -1,2 +1,69 @@
 'use strict';
 const { Worker, isMainThread, parentPort,workerData } = require('worker_threads');
+const axios = require('axios');
+const api = axios.create({})
+var url = "mongodb://localhost:27017/";
+var MongoClient = require('mongodb');
+var conn = MongoClient.connect(url,{ useUnifiedTopology: true })
+// var mongoose = require("mongoose")
+var query = '';
+
+const getParams = (options) => {
+    let txt = ""
+    let index = 0;
+    for (const key in options) {
+        txt += `${key}=${options[key]}`
+        if (index !== (Object.keys(options).length - 1)) txt += "&"
+        index++;
+    }
+    // console.log(txt)
+    return txt
+  }
+  
+  let sentToOperator = async (obj)=>{
+    let params_str = getParams(obj)
+    let status = null;
+    // console.log(params_str)
+    await api.get(`http://127.0.0.1:2345/?${params_str}`,{ proxy: { host: '127.0.0.1', port: 2345 } }).then(function (response){
+      const payload = {
+        status: response.data.status,
+        message: '',
+        operater: 'AIS'
+      }
+    try {
+        conn.then(function (client){ 
+            var db = client.db("SMS");
+            let collection = db.collection('smid')
+            var myquery = { messages: obj.messages};
+            var newvalues = {$set: {status: payload.status} };
+            let insertSMID = collection.updateOne(myquery,newvalues);
+            }
+        )
+    } catch (err) {
+        console.log('db',err);
+    } finally{
+        // query.close();
+    }
+
+    }).catch(function (error) {
+      // handle error
+      console.log('api',error);
+    })    
+  }
+
+
+// var db = null;
+
+// MongoClient.connect('url', function(err, client) {
+//     if(err) { console.error(err) }
+//     db = client.db('SMS') // once connected, assign the connection to the global variable
+// })
+
+for (let i = 0; i < workerData.data.length; i++) {
+    sentToOperator(workerData.data[i])
+}
+
+
+
+
+parentPort.postMessage(workerData.data);
--- Controller/worker.js
+++ Controller/worker.js
@@ -9,44 +9,46 @@ var url = "mongodb://localhost:27017";
 var MongoClient = require('mongodb').MongoClient;
 
 
-const getParams = (options) => {
-  let txt = ""
-  let index = 0;
-  for (const key in options) {
-      txt += `${key}=${options[key]}`
-      if (index !== (Object.keys(options).length - 1)) txt += "&"
-      index++;
-  }
-  // console.log(txt)
-  return txt
-}
+// const getParams = (options) => {
+//   let txt = ""
+//   let index = 0;
+//   for (const key in options) {
+//       txt += `${key}=${options[key]}`
+//       if (index !== (Object.keys(options).length - 1)) txt += "&"
+//       index++;
+//   }
+//   // console.log(txt)
+//   return txt
+// }
 
-let sentToOperator = (query,obj)=>{
-  let params_str = getParams(obj)
-  // console.log(params_str)
-  api.get(`http://localhost:3000/?${params_str}`).then(function (response){
-    const payload = {
-      status: response.data.status,
-      message: '',
-      operater: 'AIS'
-    }
-    // console.log(query)
-    try {
-          var db = query.db("SMS");
-          let collection = db.collection('smid');
-          var myquery = { messages: obj.messages};
-          var newvalues = {$set: {status: payload.status} };
-          let insertSMID = collection.updateOne(myquery,newvalues);
-      } catch (err) {
-          console.log(err);
-      } finally{
-          // query.close();
-      }
-  }).catch(function (error) {
-    // handle error
-    console.log(error.code);
-  })
-}
+// let sentToOperator = (query,obj)=>{
+//   let params_str = getParams(obj)
+//   // console.log(params_str)
+//   api.get(`http://localhost:3000/?${params_str}`).then(function (response){
+//     const payload = {
+//       status: response.data.status,
+//       message: '',
+//       operater: 'AIS'
+//     }
+//     // console.log(query)
+//     try {
+//           var db = query.db("SMS");
+//           let collection = db.collection('smid');
+//           var myquery = { messages: obj.messages};
+//           var newvalues = {$set: {status: payload.status} };
+//           let insertSMID = collection.updateOne(myquery,newvalues);
+//       } catch (err) {
+//           console.log(err);
+//       } finally{
+//           // query.close();
+//       }
+//   }).catch(function (error) {
+//     // handle error
+//     console.log(error.code);
+//   })
+// }
+
+
 
 let  operator = async (obj)=>{
   let data = JSON.parse(obj)
@@ -54,21 +56,27 @@ let  operator = async (obj)=>{
   var query = await MongoClient.connect(url,{useNewUrlParser: true, useUnifiedTopology: true})
   const threads = new Set();
 
-    // if (isMainThread) {
+    if (isMainThread) {
       for (let i = 0;i<chunk.length;i++){
-        for (let j =0;j<chunk[i].length;j++){
-          // console.log(chunk[i][j].messages);
-          sentToOperator(query,chunk[i][j])
-        }
+        threads.add(new Worker('./Controller/threads.js', { workerData: {data: chunk[i]}}));
       }
-      // Listen for messages from the worker and print them.
-      // worker.on('message', (msg) => { console.log(msg); });
-    // } else {
+      for (let worker of threads) {
+        worker.on('error', (err) => { throw err; });
+        worker.on('exit', () => {
+          threads.delete(worker);
+          console.log(`Thread exiting, ${threads.size} running...`);
+        })
+        worker.on('message', (msg) => {
+          console.log(msg)
+        });
+      }
+
+    } else {
       // This code is executed in the worker and not in the main thread.
       // console.log(isMainThread)
       // Send a message to the main thread.
       // parentPort.postMessage('Hello');
-    // }
+    }
   }
 
 module.exports = operator
